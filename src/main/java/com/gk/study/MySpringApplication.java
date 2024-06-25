5feab007a7cb0b31b72be11f9742ecbfcc49c436
NingF324 <2283451158@qq.com>
2024-06-25 14:14:08 +0800

    init

---
+++ src/main/java/com/gk/study/MySpringApplication.java
@@ -0,0 +1,21 @@
+package com.gk.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lengqin1024(微信)
+ * @email net936@163.com
+ */
+@SpringBootApplication
+public class MySpringApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(MySpringApplication.class, args);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
